15ad08d8df84de3a39347bb01ded3a575ff199c7
Juri Wiens <juriwiens@gmail.com>
2018-06-20 18:42:07 +0200

    Adapt example

--- src/startExample.ts
+++ src/startExample.ts
@@ -1,8 +1,13 @@
 import {BrokerService} from "./";
 
 async function start(): Promise<void> {
-    const service = new BrokerService();
-    return service.start();
+    const service = new BrokerService({port: 3000});
+
+    service.onAny(console.log);
+    service.clientManager.onAny(console.log);
+
+    await service.start();
+    console.log("Broker is listening", service.fastify.server.address());
 }
 
 start();
